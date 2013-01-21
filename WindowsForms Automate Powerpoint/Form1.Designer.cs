d9e820d60dbfaba653aac858b9cdeab111cbfdcc
tbrams <torben@brams.dk>
2013-01-21 14:59:34 +0900

    updates

---
+++ WindowsForms Automate Powerpoint/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WindowsForms_Automate_Powerpoint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonStrip = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.buttonBackup = new System.Windows.Forms.Button();
+            this.labelComments = new System.Windows.Forms.Label();
+            this.textboxComments = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(34, 14);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(110, 43);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "Open";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonStrip
+            // 
+            this.buttonStrip.Location = new System.Drawing.Point(34, 141);
+            this.buttonStrip.Name = "buttonStrip";
+            this.buttonStrip.Size = new System.Drawing.Size(110, 43);
+            this.buttonStrip.TabIndex = 3;
+            this.buttonStrip.Text = "Strip";
+            this.buttonStrip.UseVisualStyleBackColor = true;
+            this.buttonStrip.Click += new System.EventHandler(this.buttonStrip_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // buttonBackup
+            // 
+            this.buttonBackup.Location = new System.Drawing.Point(34, 92);
+            this.buttonBackup.Name = "buttonBackup";
+            this.buttonBackup.Size = new System.Drawing.Size(110, 43);
+            this.buttonBackup.TabIndex = 6;
+            this.buttonBackup.Text = "Backup";
+            this.buttonBackup.UseVisualStyleBackColor = true;
+            this.buttonBackup.Click += new System.EventHandler(this.buttonBackup_Click);
+            // 
+            // labelComments
+            // 
+            this.labelComments.AutoSize = true;
+            this.labelComments.Location = new System.Drawing.Point(544, 120);
+            this.labelComments.Name = "labelComments";
+            this.labelComments.Size = new System.Drawing.Size(78, 17);
+            this.labelComments.TabIndex = 7;
+            this.labelComments.Text = "Comments:";
+            // 
+            // textboxComments
+            // 
+            this.textboxComments.Location = new System.Drawing.Point(164, 14);
+            this.textboxComments.Name = "textboxComments";
+            this.textboxComments.Size = new System.Drawing.Size(693, 229);
+            this.textboxComments.TabIndex = 8;
+            this.textboxComments.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Comments:";
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Location = new System.Drawing.Point(34, 200);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(110, 43);
+            this.buttonQuit.TabIndex = 10;
+            this.buttonQuit.Text = "Quit";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 261);
+            this.Controls.Add(this.buttonQuit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textboxComments);
+            this.Controls.Add(this.labelComments);
+            this.Controls.Add(this.buttonBackup);
+            this.Controls.Add(this.buttonStrip);
+            this.Controls.Add(this.openFileButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PowerPoint Cleanup";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonStrip;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button buttonBackup;
+        private System.Windows.Forms.RichTextBox textboxComments;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelComments;
+        private System.Windows.Forms.Button buttonQuit;
+    }
+}
+
---
+++ WindowsForms Automate Powerpoint/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Office.Core;
+using Office = Microsoft.Office.Core;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using System.Runtime.InteropServices;
+using System.Reflection;
+
+
+namespace WindowsForms_Automate_Powerpoint
+{
+    public partial class Form1 : Form
+    {
+        string fileName;
+        List<string> myComments = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Powerpoint|*.ppt;*.pptx|All files|*.*";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            dialog.Title = "Please select a presentation";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName = dialog.FileName;
+
+                this.Text = this.Text + ":" + System.IO.Path.GetFileName(fileName);
+                this.Size = new Size(887,306);
+
+                listPowerPointNotes();
+            }
+            else
+                Console.WriteLine("Something went wrong reading the filename from the Dialog");
+
+            buttonQuit.Visible = true;
+        }
+
+        private void listPowerPointNotes()
+        {
+            PowerPoint.Application oPowerPoint = null;
+            PowerPoint.Presentations oPres = null;
+            PowerPoint.Presentation oPre = null;
+
+            //
+            try
+            {
+                oPowerPoint = new PowerPoint.Application();
+
+                // By default PowerPoint is invisible, till you make it visible.
+                // oPowerPoint.Visible = Office.MsoTriState.msoTrue;
+
+                oPres = oPowerPoint.Presentations;
+                oPre = oPres.Open(fileName, MsoTriState.msoTrue, WithWindow: MsoTriState.msoFalse);
+
+                int slidenum = 0;
+
+                // Set cursor to indicate this may take time...
+                Cursor.Current = Cursors.WaitCursor;
+                foreach (PowerPoint.Slide mySlide in oPre.Slides)
+                {
+                    slidenum++;
+                    string s = mySlide.NotesPage.Shapes[2].TextFrame.TextRange.Text;
+
+                    if (s != "")
+                    {
+                        myComments.Add("Slide: " + slidenum);
+                        myComments.Add(s);
+                        myComments.Add("");
+                        myComments.Add("");
+                    }
+
+                    // this is for removing comments:
+                    // mySlide.NotesPage.Shapes[2].TextFrame.TextRange.Text = "removed ....";
+                }
+
+
+                if (slidenum!=0) {
+                    foreach (string myString in myComments)
+                    {
+                        textboxComments.Text += myString + Environment.NewLine;
+                    }
+
+                    label1.Visible = true;
+                    textboxComments.Visible = true;
+                    buttonBackup.Visible = true;
+                    buttonStrip.Visible=true;
+                }
+
+                // reset cursor 
+                Cursor.Current = Cursors.Default;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Trying to open Powerpoint throws the error: {0}",
+                    ex.Message);
+            } 
+
+        }
+
+
+ 
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            // Get file name.
+            string name = saveFileDialog1.FileName;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textboxComments.Visible = false;
+            label1.Visible = false;
+            buttonBackup.Visible = false;
+            buttonStrip.Visible = false;
+            buttonQuit.Visible = false;
+
+        }
+
+        private void buttonBackup_Click(object sender, EventArgs e)
+        {
+            // FIRST: Get file name from dialog
+
+            // Set the properties on SaveFileDialog1 so the user is  
+            // prompted to overwrite the file if it does exist.
+            // -- SaveFileDialog1.CreatePrompt = true;
+            saveFileDialog1.OverwritePrompt = true;
+
+            // Set the file name to new presentation.pptx, set the type filter 
+            // to PowerPoint files, and set the initial directory to the  
+            // Desktop folder.
+            saveFileDialog1.FileName = "slide notes backup";
+            // DefaultExt is only used when "All files" is selected from  
+            // the filter box and no extension is specified by the user.
+            saveFileDialog1.DefaultExt = "txt";
+            saveFileDialog1.Filter =
+                "Text (*.txt)|*.txt;|All files|*.*";
+            saveFileDialog1.InitialDirectory =
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            // Call ShowDialog and check for a return value of DialogResult.OK, 
+            // which indicates that the file was saved. 
+            DialogResult result = saveFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {       
+                // save content of comment array to file
+
+                StreamWriter file = new System.IO.StreamWriter(saveFileDialog1.FileName);
+                foreach (string line in myComments)
+                    file.WriteLine(line);
+                file.Close();
+            }
+            buttonBackup.Enabled = false;
+         }
+
+        private void buttonStrip_Click(object sender, EventArgs e)
+        {
+            // remove all slide notes and save clean copy named: "..._stripped.pptx"
+
+            PowerPoint.Application oPowerPoint = null;
+            PowerPoint.Presentations oPres = null;
+            PowerPoint.Presentation oPre = null;
+
+            //
+            try
+            {
+                oPowerPoint = new PowerPoint.Application();
+
+                // By default PowerPoint is invisible, till you make it visible.
+                // oPowerPoint.Visible = Office.MsoTriState.msoTrue;
+
+                oPres = oPowerPoint.Presentations;
+                oPre = oPres.Open(fileName, ReadOnly:MsoTriState.msoFalse, WithWindow:MsoTriState.msoFalse);
+
+                // Set cursor to indicate this may take time...
+                Cursor.Current = Cursors.WaitCursor;
+
+                foreach (PowerPoint.Slide mySlide in oPre.Slides)
+                {
+                    string s = mySlide.NotesPage.Shapes[2].TextFrame.TextRange.Text;
+                    if (s != "")
+                    {
+                        mySlide.NotesPage.Shapes[2].TextFrame.TextRange.Text = "";
+                    }
+                }
+
+                string shortFileName= System.IO.Path.GetFileName(fileName);
+                string saveDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                int decimalPoint = shortFileName.LastIndexOf(".");
+                string fileType = shortFileName.Substring(decimalPoint);
+                string sFileName = shortFileName.Substring(0, decimalPoint);
+                string newFileName = saveDirectory + "\\" + sFileName + "_stripped" + fileType;
+
+                oPre.SaveAs(newFileName);
+
+                // reset cursor 
+                Cursor.Current = Cursors.Default;
+                buttonStrip.Enabled = false;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Trying to open Powerpoint throws the error: {0}",
+                    ex.Message);
+            }
+
+        }
+
+        private void clearPowerPointNotes()
+        {
+            PowerPoint.Application oPowerPoint = null;
+            PowerPoint.Presentations oPres = null;
+            PowerPoint.Presentation oPre = null;
+
+            //
+            try
+            {
+                oPowerPoint = new PowerPoint.Application();
+
+                // By default PowerPoint is invisible, till you make it visible.
+                // oPowerPoint.Visible = Office.MsoTriState.msoTrue;
+
+                oPres = oPowerPoint.Presentations;
+                oPre = oPres.Open(fileName, MsoTriState.msoTrue, WithWindow: MsoTriState.msoFalse);
+
+                foreach (PowerPoint.Slide mySlide in oPre.Slides)
+                {
+                    string s = mySlide.NotesPage.Shapes[2].TextFrame.TextRange.Text;
+                    if (s != "")
+                    {
+                        mySlide.NotesPage.Shapes[2].TextFrame.TextRange.Text = "removed ....";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Trying to open Powerpoint throws the error: {0}", ex.Message);
+            }
+
+            oPre.Save();
+
+            //
+        }
+
+        private void buttonQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
